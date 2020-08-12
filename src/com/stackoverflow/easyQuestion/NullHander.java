f03f5a7f96749931bb38e3ac9782a4694d5b8060
alexdyysp <alexdyysp@126.com>
2020-08-12 21:36:04 +0800

    so

---
+++ src/com/stackoverflow/easyQuestion/NullHander.java
@@ -0,0 +1,9 @@
+package com.stackoverflow.easyQuestion;
+
+import java.util.Objects;
+
+public class NullHander {
+    public static void main(String[] args){
+        System.out.println(Objects.isNull(null));
+    }
+}
